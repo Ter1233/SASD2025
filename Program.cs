d22eebd9e31ed49e4487fb7cba025c3baa1a9ad6
K4N <bunyaveedeechuay@gmail.com>
2026-01-09 15:52:36 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+namespace Restaurant
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  Patcharapon Phetchoochart ID 66102010146
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            if (args.Length > 0 && args[0].ToLower() == "-k")
+                Application.Run(new KitchenForm());
+            else
+                Application.Run(new Waiterform());
+            { }
+
+        
+        }
+    }
+}
---
+++ Waiterform.cs
@@ -0,0 +1,15 @@
+namespace Restaurant
+{
+    public partial class Waiterform : Form
+    {
+        public Waiterform()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
